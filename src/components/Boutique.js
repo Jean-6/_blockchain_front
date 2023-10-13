bfb55a89eab8ed5beb24b47dc676021125ee9684
David <zdavid1@myges.fr>
2023-10-13 22:32:04 +0200

    reveal

--- src/components/Boutique.js
+++ src/components/Boutique.js
@@ -4,6 +4,8 @@ import SideNav from "./SideNav.js";
 import RevealCards from "./RevealCards.js";
 import Web3 from 'web3';
 import abiJson from '../abi.json';
+import Loader from "./Loader.js";
+
 const network = 'https://eth-sepolia.g.alchemy.com/v2/ABRLHWqw7mF6DYgLygNgwsUpMnQ5XGh5';
 const web3 = new Web3(new Web3.providers.HttpProvider(network));
 const contractAddress = '0x72C08dC1d6499e1235323B781A2Dc5C650EDf9F6';
@@ -12,14 +14,19 @@ const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
 const accountAddress = '0xec21c4104eca69655f7d5e959df27489382d0447';  // Your Ethereum account address
 
 
-function RenderBoutique({handleBuyClick}) {
+function RenderBoutique({handleBuyClick, loading}) {
+
     return (
         <div className="global-cards">
             <h1 className="title-boutique">Boutique</h1>
             <div className="shop-all">
                 <div className="shop">
                     <img src="cartes.png" alt="cartes" className="image-cartes"/>
-                    <input type="button" value="Acheter" onClick={handleBuyClick}/>
+                    {loading ? (
+                        <Loader/>
+                    ) : (
+                        <input type="button" value="Acheter" onClick={handleBuyClick}/>
+                    )}
                 </div>
             </div>
         </div>
@@ -28,9 +35,10 @@ function RenderBoutique({handleBuyClick}) {
 
 function Boutique() {
     const [displayShop, setdisplayShop] = useState(true);
-
+    const [loading, setLoading] = useState(false);
     const handleBuyClick = async () => {
         try {
+            setLoading(true);
             const functionCallData = contractInstance.methods.openPack().encodeABI();
             const transactionObject = {
                 to: contractAddress,
@@ -43,8 +51,10 @@ function Boutique() {
             const signedTx = await web3.eth.accounts.signTransaction(transactionObject, '947727c5c4f19411fbaae0bbe96ea0811d3d2729175cfa9a8287e92ad9686c06');
             const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
             console.log('Transaction receipt:', receipt);
+            setLoading(false);
             setdisplayShop(!displayShop);
         } catch (error) {
+            setLoading(false);
             console.error('Error:', error);
         }
     };
@@ -52,7 +62,7 @@ function Boutique() {
     return (
         <div className="row">
             <SideNav/>
-            {displayShop ? <RenderBoutique handleBuyClick={handleBuyClick}/> : <RevealCards/>}
+            {displayShop ? <RenderBoutique handleBuyClick={handleBuyClick} loading={loading}/> : <RevealCards/>}
         </div>
     );
 }
