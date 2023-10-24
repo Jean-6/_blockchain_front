5118d4523ae963374158c6eb1a1449e50d7d647c
Jean-Jaures <jean-jaures.oka@sncf.fr>
2023-10-24 12:26:59 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 
 import './App.css';
+import MetaMask from "./MetaMask";
 import {DynamicContextProvider, DynamicWidget, SortWallets,defaultNumberOfWalletsToShow} from "@dynamic-labs/sdk-react";
-const App = () => (
+
+const App = () =>{
+    return (
+        <MetaMask/>
+        //<p>Hello World </p>
+    );
+}
+
+/*const App = () => (
     <DynamicContextProvider
         settings={{
             environmentId: '66db0f15-5b01-4d09-b394-a89a3d11490d',
@@ -11,6 +20,7 @@ const App = () => (
     >
         <DynamicWidget />
     </DynamicContextProvider>
-);
+);*/
+
 
 export default App;
