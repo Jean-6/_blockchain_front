665f5375d88cef872399e7ab958befd8227a9aa6
David <zdavid1@myges.fr>
2023-11-18 20:57:06 +0100

    auth moralis

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import './styles/App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Cards from "./components/Cards.js";
 import Boutique from "./components/Boutique.js";
 import Duel from "./components/Duel.js";
+import {createConfig, configureChains, WagmiConfig} from "wagmi";
+import {publicProvider} from "wagmi/providers/public";
+import {mainnet} from "wagmi/chains";
+import SignIn from "./components/Signin.js";
+import User from "./components/User.js";
+import React from "react";
+
+const {publicClient, webSocketPublicClient} = configureChains(
+    [mainnet],
+    [publicProvider()]
+);
+
+const config = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient,
+});
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <SignIn/>,
+    },
+    {
+        path: "/signin",
+        element: <SignIn/>,
+    },
+    {
+        path: "/duel",
+        element: <Duel/>,
+    },
+    {
+        path: "/cards",
+        element: <Cards/>,
+    },
+    {
+        path: "/boutique",
+        element: <Boutique/>,
+    },
+    {
+        path: "/user",
+        element: <User/>,
+    },
+]);
+
 
 function App() {
     return (
-        <Router>
-            <div>
-                <Routes>
-                    <Route path="/" element={<Cards/>}/>
-                    <Route path="/duel" element={<Duel/>}/>
-                    <Route path="/cards" element={<Cards/>}/>
-                    <Route path="/boutique" element={<Boutique/>}/>
-                </Routes>
-            </div>
-        </Router>
+        <WagmiConfig config={config}>
+            <RouterProvider router={router}/>
+        </WagmiConfig>
     );
 }
 
